138c4786d313af0a99065a7a6b04f272274ea856
Luciano Fernandez <luchofseven@gmail.com>
2022-05-24 19:51:39 -0300

    agregando un poco de estilos

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,8 +1,9 @@
 import logo from '../images/favicon.png'
+import estilos from '../styles/styles.css'
 
-export const NavBar = () => {
+const NavBar = () => {
   return (
-    <nav className="navbar navbar-dark navbar-expand-lg bg-dark">
+    <nav className="navbar navbar-light navbar-expand-lg bg-alpha-color-one">
       <div className="container-fluid">
         <a className="navbar-brand" href="#">
           <img src={logo} alt="floora logo" height="50px" width="50px"/>
@@ -19,31 +20,27 @@ export const NavBar = () => {
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav">
+          <ul className="navbar-nav mi-estilo-nav">
             <li className="nav-item">
-              <a className="nav-link" aria-current="page" href="#">
-                Inicio
-              </a>
+              <a className="nav-link" aria-current="page" href="#">INICIO</a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#">
-                Productos
-              </a>
+              <a className="nav-link" href="#">PRODUCTOS</a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#">
-                Promociones
-              </a>
+              <a className="nav-link" href="#">PROMOCIONES</a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#">Nosotros</a>
+              <a className="nav-link" href="#">NOSOTROS</a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#">Contacto</a>
+              <a className="nav-link" href="#">CONTACTO</a>
             </li>
           </ul>
         </div>
       </div>
     </nav>
-  );
-};
+  )
+}
+
+export default NavBar
