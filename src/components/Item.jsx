ad65bb90c804360543efec99aed81ec6bf9913e3
Luciano Fernandez <luchofseven@gmail.com>
2022-07-03 21:15:24 -0300

    arreglo del navbar bootstrap

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -3,13 +3,15 @@ import { Link } from "react-router-dom";
 export default function Item({ item }) {
 
   return (
-    <div className="card mi-estilo-card" style={{ width: "18rem" }}>
-      <img src={item.img} className="card-img-top" alt={item.name} />
-      <div className="card-body">
-        <h5 className="card-title">{item.name}</h5>
-        <h4 className="p-4">${item.price}</h4>
-        <Link to={`/producto/${item.id}`} className="btn mi-estilo-btn">Ver más</Link>
+    <Link className="mi-estilo-itemLink" to={`/producto/${item.id}`}>
+      <div className="card mi-estilo-card" style={{ width: "18rem" }}>
+        <img src={item.img} className="card-img-top" alt={item.name} />
+        <div className="card-body">
+          <h5 className="card-title">{item.name}</h5>
+          <h4 className="p-4">${item.price}</h4>
+          <button className="btn mi-estilo-btn">Ver más</button>
+        </div>
       </div>
-    </div>
+    </Link>
   )
 }
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,27 +1,37 @@
+import { useRef } from "react";
 import { Link } from "react-router-dom"
 import CartWidget from "./CartWidget.jsx"
 
 export default function NavBar() {
 
+  //Solución del NavBar Bootstrap facilitada por un compañero de la cursada.
+  const navButton = useRef(null);
+  const linksContainerRef = useRef(null);
+
+  function collapseNav() {
+    navButton.current.classList.add("collapsed");
+    linksContainerRef.current.classList.remove("show");
+  }
+
   return (
     <nav className="navbar navbar-light navbar-expand-lg bg-alpha-color-one">
       <div className="container-fluid">
-        <Link className="navbar-brand" to="/">
+        <Link onClick={collapseNav} className="navbar-brand" to="/">
           <img src="/images/favicon.png" className="mi-estilo-logo" alt="floora logo" height="65px" width="65px" />
         </Link>
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation" >
+        <button ref={navButton} className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation" >
           <span className="navbar-toggler-icon"></span>
         </button>
-        <div className="collapse navbar-collapse" id="navbarNav">
+        <div ref={linksContainerRef} className="collapse navbar-collapse" id="navbarNav">
           <ul className="navbar-nav mi-estilo-nav">
             <li className="nav-item">
-              <Link className="nav-link" to="/cart"><CartWidget /></Link>
+              <Link onClick={collapseNav} className="nav-link" to="/cart"><CartWidget /></Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link" to="/">INICIO</Link>
+              <Link onClick={collapseNav} className="nav-link" to="/">INICIO</Link>
             </li>
             <li className="nav-item mi-estilo-link">
-              <Link className="nav-link" to="/aboutus">NOSOTROS</Link>
+              <Link onClick={collapseNav} className="nav-link" to="/aboutus">NOSOTROS</Link>
             </li>
           </ul>
         </div>
